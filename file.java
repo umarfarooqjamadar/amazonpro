773c2aa0522abfd6d7ddd4c6f398972792173157
Cloud User <ec2-user@ip-172-31-13-230.ap-south-1.compute.internal>
2020-06-28 09:47:02 +0000

    added

---
+++ file.java
@@ -0,0 +1,15 @@
+
+public class file{
+
+   public static void main(String[] args){
+
+        for(int i=1;i<=10;i++){
+
+           System.out.println("hello world..."+i);
+
+             }
+
+        }
+
+    }
+
